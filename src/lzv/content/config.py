e73c9346fa4b7d534ebefc4b91b7834fb9ab5795
Peter Reimer <reimer@hbz-nrw.de>
2021-12-06 08:44:47 +0100

    more fields and widgets

--- src/lzv/content/config.py
+++ src/lzv/content/config.py
@@ -4,3 +4,28 @@ SERVICESTATUS = (
     ("planning", u"In Planung"),
     ("unavailable", u"nicht verfügbar"),
 )
+BASEFUNCTIONS = (
+    ("function1", u"Quellsystemanbindung"),
+    ("function2", u"Automatisierte Workflows"),
+    ("function3", u"Vorkonfigurierte Materialflows"),
+    ("function4", u"Individuelles Preservation Planning"),
+    ("function5", u"Reporting"),
+)
+EXTRAFUNCTIONS = (
+    ("extra1", u"Rosetta Webzugriff"),
+    ("extra2", u"Rosetta Systemzugriff"),
+    ("extra3", u"Modifikation der Materialflows"),
+    ("extra4", u"Individuelles Rechtemanagement"),
+)
+INGESTS = (
+    ("ingest1", u"Automatische Einlieferung über OAI-PMH"),
+    ("ingest2", u"Automatische Einlieferung über Submission-Application"),
+    ("ingest3", u"Automatische Einlieferung über REST"),
+    ("ingest4", u"Automatische Einlieferung über CSV"),
+    ("ingest5", u"Manuelle Einlieferung nach Vereinbarung"),
+)
+STORAGES = (
+    ("storage1", u"Zentrale Speicherung"),
+    ("storage2", u"Lokale Speicherung"),
+    ("storage4", u"Hybride Speicherung"),
+)
--- src/lzv/content/service.py
+++ src/lzv/content/service.py
@@ -7,10 +7,22 @@ from plone.supermodel import model
 from zope import schema
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from Products.Five import BrowserView
-from lzv.content.config import SERVICESTATUS
+from lzv.content.config import SERVICESTATUS, BASEFUNCTIONS, EXTRAFUNCTIONS, INGESTS, STORAGES
 
 items = [SimpleTerm(value=key, title=value) for (key, value) in SERVICESTATUS]
 choice_servicesstatus = SimpleVocabulary(items)
+
+items = [SimpleTerm(value=key, title=value) for (key, value) in BASEFUNCTIONS]
+choice_basefunctions = SimpleVocabulary(items)
+
+items = [SimpleTerm(value=key, title=value) for (key, value) in EXTRAFUNCTIONS]
+choice_extrafunctions = SimpleVocabulary(items)
+
+items = [SimpleTerm(value=key, title=value) for (key, value) in INGESTS]
+choice_ingests = SimpleVocabulary(items)
+
+items = [SimpleTerm(value=key, title=value) for (key, value) in STORAGES]
+choice_storages = SimpleVocabulary(items)
 
 
 class IService(model.Schema):
@@ -33,7 +45,6 @@ class IService(model.Schema):
         description=_("Kürzel des Services.")
     )
 
-
     details = RichText(
         title=_(u"Detailbeschreibung"),
         description=_("Detailierte Beschreibung des Services.")
@@ -55,27 +66,44 @@ class IService(model.Schema):
 
     fieldset('options',
         label=u'Serviceumfang und Optionen',
-        fields=['basefunctions']
+        fields=['basefunctions', 'extrafunctions', 'ingests', 'serviceoptions', 'storageoptions']
     )
 
     basefunctions = schema.List(
         title=_(u"Basisfunktionen"),
         description=_("Grundlegende Funktionen des Services."),
-        value_type=schema.Choice(
-            [
-            "Quellsystemanbindung",
-            "Automatisierte Workflows",
-            "Vorkonfigurierte Materialflows",
-            "Individuelles Preservation Planning",
-            "Reporting"
-            ]
-        )
+        value_type=schema.Choice(vocabulary=choice_basefunctions)
     )
+
+    extrafunctions = schema.List(
+        title=_(u"Zusatzfunktionen"),
+        description=_("Weitergehende Funktionen des Services."),
+        value_type=schema.Choice(vocabulary=choice_extrafunctions),
+        required=False
+    )
+
+    ingests = schema.List(
+        title=_(u"Einlieferung"),
+        description=_("Mögliche Methoden zur Einlieferung."),
+        value_type=schema.Choice(vocabulary=choice_ingests)
+    )
+
+    serviceoptions = RichText(
+        title=_(u"Service-Optionen"),
+        description=_("Mögliche Optionen des Services.")
+    )
+
+    storageoptions = schema.List(
+        title=_(u"Speicher-Optionen"),
+        description=_("Mögliche Speicheroptionen."),
+        value_type=schema.Choice(vocabulary=choice_storages)
+    )
+
     # Nutzungsvoraussetzungen
 
     fieldset('terms',
         label=u'Nutzungsvoraussetzungen',
-        fields=['staff']
+        fields=['staff', 'terms']
     )
 
     staff = schema.List(
@@ -83,6 +111,12 @@ class IService(model.Schema):
         description=_("Notwendiges Personal, eine Stellenbeschreibung pro Zeile."),
         value_type=schema.TextLine(required=True)
     )
+
+    terms = RichText(
+        title=_(u"Nutzungsregelungen"),
+        description=_("Nutzungsregelungen")
+    )
+
     # Portlet
 
     fieldset('portlet',
@@ -117,6 +151,17 @@ class ServiceView(BrowserView):
 
     def get_servicestatus_names(self):
         return  {k:v for k,v in SERVICESTATUS}
+    
+    def get_basefunctions_names(self):
+        return  {k:v for k,v in BASEFUNCTIONS}
+    
+    def get_extrafunctions_names(self):
+        return  {k:v for k,v in EXTRAFUNCTIONS}
 
+    def get_ingests_names(self):
+        return  {k:v for k,v in INGESTS}
+    
+    def get_storages_names(self):
+        return  {k:v for k,v in STORAGES}
 
 
