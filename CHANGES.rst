23298d9619a3f8e87c20d952d4c01e02f6bc52cd
Peter Reimer <reimer@hbz-nrw.de>
2022-02-14 09:25:35 +0100

    minor version bump

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,11 @@ Changelog
 =========
 
 
+1.0a2 (unreleased)
+------------------
+
+- Portlet mit Kontaktinformationen
+
 1.0a1 (unreleased)
 ------------------
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='lzv.content',
-    version='1.0a1',
+    version='1.0a2',
     description="LZV content types",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
