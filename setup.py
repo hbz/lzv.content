48d559c50fd328670ac11888cdd897e254da12d2
Peter Reimer <reimer@hbz-nrw.de>
2022-06-23 18:11:58 +0200

    minor version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='lzv.content',
-    version='1.0a6',
+    version='1.0',
     description="LZV content types",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
